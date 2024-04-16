f237e3296176fd057299bd8c6e701d020bdbe89c
James Boulton <james@dashio.io>
2024-04-16 13:09:54 +1200

    Edit parts

--- db_manager/gui.py
+++ db_manager/gui.py
@@ -43,12 +43,15 @@ class mainGUI():
         # Retrieve values from entry widgets and combobox
         values = [self.entries[field].get() for field in self.entries]
         component_type = self.component_type_combobox.get()
-        values.insert(1, component_type)  # Insert component_type at index 1
-
+        values.append(component_type)  # Insert component_type at index 1
+        print(values)
         # Insert values into the database
-        sql = """INSERT INTO parts (description, component_type, datasheet, footprint_ref,
+        if values[2] == '' or values[3] == '' or values[4] == '' or values[5] == '':
+            messagebox.showerror("Error", "Need to have KiCAD Part Number, Man Part Number, Footprint Ref, Symbol Ref.")
+            return
+        sql = """INSERT INTO parts (description, datasheet, footprint_ref,
                 symbol_ref, model_ref, kicad_part_number, manufacturer_part_number,
-                manufacturer, manufacturer_part_url, note, value)
+                manufacturer, manufacturer_part_url, note, value, component_type)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
         self.cursor.execute(sql, values)
         self.db_connection.commit()
@@ -68,6 +71,7 @@ class mainGUI():
 
         # Insert parts into the Treeview
         for part in parts:
+            print(part)
             self.tree.insert("", "end", text=part[0], values=(
                 part[1], part[2], part[3], part[4], part[5], part[6], part[7]), tags="center")
 
@@ -86,13 +90,15 @@ class mainGUI():
 
         # Create button to add supplier
         self.add_supplier_button = ttk.Button(
-            self.add_supplier_window, text="Add Supplier", command=self._add_supplier)
+            self.add_supplier_window,
+            text="Add Supplier",
+            command=self._add_supplier
+        )
         self.add_supplier_button.grid(row=len(fields), column=0, columnspan=2, pady=5)
 
     def _add_supplier(self):
         # Retrieve values from entry widgets
-        values = [self.supplier_entries[field].get()
-                  for field in self.supplier_entries]
+        values = [self.supplier_entries[field].get() for field in self.supplier_entries]
 
         # Insert values into the database
         sql = """INSERT INTO supplier (supplier_name, supplier_address, supplier_web_url, supplier_phone, supplier_email) VALUES (%s, %s, %s, %s, %s)"""
@@ -162,17 +168,26 @@ class mainGUI():
         # Open a window to edit the selected part
         selected_part_id = selected_item[0]  # Get the ID of the selected item
         selected_part = self.tree.item(selected_part_id)
-        part_details = selected_part["values"]
+        kicad_part_number = selected_part["text"]
+
+        # Fetch current values from the database
+        sql = """SELECT description, datasheet, footprint_ref,
+                symbol_ref, model_ref, manufacturer_part_number,
+                manufacturer, manufacturer_part_url, note, value, component_type
+                FROM parts WHERE kicad_part_number = %s"""
+        self.cursor.execute(sql, (kicad_part_number,))
+        part_details = self.cursor.fetchone()
 
         # Open an edit window with the details of the selected part pre-filled
-        self.edit_part_window = tk.Toplevel(self.root)
-        self.edit_part_window.title("Edit Part")
+        self.edit_part_window = ttk.Toplevel("Edit Part")
 
+        # Configure resizing behavior
         self.edit_part_window.columnconfigure(1, weight=1)
+
         # Create and pack labels and entry widgets for input fields
         fields = [
-            "Description", "Component Type", "Datasheet", "Footprint Ref",
-            "Symbol Ref", "Model Ref", "KiCad Part Number", "Manufacturer Part Number",
+            "Description", "Datasheet", "Footprint Ref",
+            "Symbol Ref", "Model Ref", "Manufacturer Part Number",
             "Manufacturer", "Manufacturer Part URL", "Note", "Value"
         ]
         self.entries = {}
@@ -181,35 +196,62 @@ class mainGUI():
             entry = ttk.Entry(self.edit_part_window)
             entry.grid(row=i, column=1, sticky="ew")
             # Pre-fill entry with existing value if available
-            if i < len(part_details):
+            if part_details:
                 entry.insert(0, part_details[i])
             self.entries[field] = entry
 
+        # Create selector for Component Type
+        ttk.Label(self.edit_part_window, text="Component Type").grid(row=len(fields), column=0, sticky="e")
+        self.component_type_combobox = ttk.Combobox(self.edit_part_window, values=[
+            '', 'Resistor', 'Capacitor', 'Connector', 'Diode', 'Electro Mechanical',
+            'Mechanical', 'Inductor', 'Opto', 'OpAmp', 'Transister', 'Power Supply IC', 'Semiconductor'
+        ])
+        self.component_type_combobox.grid(row=len(fields), column=1, sticky="ew")
+        if part_details:
+            self.component_type_combobox.set(part_details[-1])  # Pre-select existing component type
+
         # Create button to update part
         self.update_part_button = ttk.Button(self.edit_part_window, text="Update Part", command=self._update_part)
-        self.update_part_button.grid(row=len(fields), column=0, columnspan=2)
+        self.update_part_button.grid(row=len(fields) + 1, column=0, columnspan=2, pady=5)
 
     def _update_part(self):
         # Retrieve values from entry widgets
         values = [self.entries[field].get() for field in self.entries]
 
-        # Get the selected item from the Treeview
+        # Retrieve value from combobox for Component Type
+        component_type = self.component_type_combobox.get()
+        values.append(component_type)  # Append component_type to the values list
+
+        # Check if required fields are not empty
+        required_fields = {
+            "Footprint Ref": values[2],
+            "Symbol Ref": values[3],
+            "Manufacturer Part Number": values[6]
+        }
+        for field, value in required_fields.items():
+            if not value.strip():
+                messagebox.showerror("Error", f"{field} cannot be empty.")
+                return
+
+        # Get the KiCad Part Number of the selected item from the Treeview
         selected_item = self.tree.selection()
         selected_part_id = selected_item[0]  # Get the ID of the selected item
-
-        # Update values in the Treeview
-        self.tree.item(selected_part_id, values=values)
+        selected_kicad_part_number = self.tree.item(selected_part_id, "text")
 
         # Update values in the database
-        sql = """UPDATE parts SET description = %s, component_type = %s, datasheet = %s, footprint_ref = %s,
+        sql = """UPDATE parts SET description = %s, datasheet = %s, footprint_ref = %s,
                 symbol_ref = %s, model_ref = %s, manufacturer_part_number = %s,
-                manufacturer = %s, manufacturer_part_url = %s, note = %s, value = %s
+                manufacturer = %s, manufacturer_part_url = %s, note = %s, value = %s,
+                component_type = %s
                 WHERE kicad_part_number = %s"""
-        self.cursor.execute(sql, values[1:] + [values[0]])  # Exclude KiCad Part Number from the values
+        print(values + [selected_kicad_part_number])
+        self.cursor.execute(sql, values + [selected_kicad_part_number])  # Exclude KiCad Part Number from the values
         self.db_connection.commit()
 
         # Close the edit part window
         self.edit_part_window.destroy()
+        self.tree.delete(*self.tree.get_children())
+        self._populate_parts_tree()
 
     def __init__(self, db_connection) -> None:
         self.db_connection = db_connection
